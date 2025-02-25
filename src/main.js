88a24bb1f691505d9638e3ed99f7ed35f27c7df0
Pecas622 <santiagozorrilla5@gmail.com>
2025-02-25 17:20:43 -0300

    clase 2 y 3

--- src/main.js
+++ src/main.js
@@ -1,17 +1,35 @@
-import express from 'express'
-import usersRouter from './routes/users.routes.js'
-import mongoose from 'mongoose'
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import MongoStore from 'connect-mongo';
+import mongoose from 'mongoose';
+import usersRouter from './routes/users.routes.js';
+import sessionsRouter from './routes/sessions.routes.js';
+import cartsRouter from './routes/carts.routes.js';
 
-const app = express()
-const PORT = 8000
+const app = express();
+const PORT = 8000;
+
+app.use(express.json());
+app.use(cookieParser("firmaSecreta"));
+app.use(session({
+    store: MongoStore.create({
+        mongoUrl: "mongodb+srv://santizorrilla:Pecas622@cluster0.eazxc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+        ttl: 60
+    }),
+    secret: "sesionSecreta",
+    resave: true,
+    saveUninitialized: true
+}));
 
 mongoose.connect("mongodb+srv://santizorrilla:Pecas622@cluster0.eazxc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 .then(() => console.log("DB is connected"))
-.catch((e) => console.log(e))
+.catch((e) => console.log(e));
 
-app.use(express.json())
-app.use('/api/users', usersRouter)
+app.use('/api/users', usersRouter);
+app.use('/api/sessions', sessionsRouter);
+app.use('/api/carts', cartsRouter);
 
 app.listen(PORT, () => {
-    console.log("Server on port:",PORT);
-})
+    console.log("Server on port:", PORT);
+});
